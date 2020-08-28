0f2f99f42af14c975604fb85e7779d7b5f0df029
Johan Haleby <johan.haleby@gmail.com>
2020-08-28 19:07:31 +0200

    Fixing so that toggling languages works again

--- js/_partials/main.js
+++ js/_partials/main.js
@@ -45,14 +45,14 @@ setTimeout(function () {
 if (document.location.pathname.includes("/documentation")) {
     // "Added in" labels
     let addedTags = {
-        "validator-nullability": "3.1.0",
-        "shared-state": "3.2.0",
-        "vue-directory-location": "3.5.0",
-        "redirect-to-lowercase-path-plugin": "3.7.0",
-        "rate-limiting": "3.7.0",
-        "setting-the-host": "3.7.0",
-        "dynamic-single-page-handler": "3.7.0",
-        "cdn-webjars": "3.8.0",
+        // "validator-nullability": "3.1.0",
+        // "shared-state": "3.2.0",
+        // "vue-directory-location": "3.5.0",
+        // "redirect-to-lowercase-path-plugin": "3.7.0",
+        // "rate-limiting": "3.7.0",
+        // "setting-the-host": "3.7.0",
+        // "dynamic-single-page-handler": "3.7.0",
+        // "cdn-webjars": "3.8.0",
     };
     Object.keys(addedTags).forEach(key => {
         document.getElementById(key).classList.add("added-parent");
